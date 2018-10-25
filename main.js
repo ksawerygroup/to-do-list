0857d45c094348a0b845b22ab4591021e08d5e78
ksawerygroup <ksawerygroup@gmail.com>
2018-10-25 22:29:16 +0200

    add file directory

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const removeTask = () => {
-  console.log(event.target.textContent); //przechwycenie tego co klikneliśmy może być this ale musi być function
+const removeTask = (e) => {
+  //console.log(event.target.textContent); //przechwycenie tego co klikneliśmy może być this ale musi być function
+  // e.target.remove();
+  // e.target.parentNode.remove();//przechwytuje i usuwa rodzica 
+  const index = e.target.dataset.key; //przypisanie naciśniętego data w HTML w tym przypadku z nazwą key
+
+  document.querySelector(`li[data-key='${index}']`).remove(); //pobranie selektora li[data-key='${index}'] z odpowiednim indexem i usunięcie go;
+
 }
-document.querySelectorAll('li').forEach(item => item.addEventListener('click', removeTask));
+document.querySelectorAll('button[data-key]').forEach(item => item.addEventListener('click', removeTask));
